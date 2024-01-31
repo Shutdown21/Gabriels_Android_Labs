f3ea711ab807055ec18ea1abed79ebabdc383b09
Gabriel Hubert <hube0078@algonquinlive.com>
2024-01-31 15:26:43 -0500

    Commit 7: Finished week 2

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.hube0078.R;
 import algonquin.cst2335.hube0078.databinding.ActivityMainBinding;
@@ -33,8 +34,39 @@ public class MainActivity extends AppCompatActivity {
         model.editString.observe(this, s -> {
             variableBinding.textview.setText("Your edit text has: " + model.editString);
         });
+        model.isSelected.observe(this, seleceted ->{
+            variableBinding.checkbox.setChecked(seleceted);
+            variableBinding.radio.setChecked(seleceted);
+            variableBinding.swicth.setChecked(seleceted);
+        });
 
+        variableBinding.checkbox.setOnCheckedChangeListener((btn, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, "The value is now: " +isChecked, duration);
+            toast.show();
+        });
 
+        variableBinding.swicth.setOnCheckedChangeListener((btn, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, "The value is now: " +isChecked, duration);
+            toast.show();
+        });
 
+        variableBinding.radio.setOnCheckedChangeListener((btn, isChecked) ->{
+            model.isSelected.postValue(isChecked);
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this, "The value is now: " +isChecked, duration);
+            toast.show();
+        });
+
+        variableBinding.logo.setOnClickListener((btn) ->{});
+
+        variableBinding.myimagebutton.setOnClickListener(click ->{
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(this ,"The height is: " + click.getHeight() + " The width is : " + click.getWidth(), duration);
+            toast.show();
+        });
     }
 }
